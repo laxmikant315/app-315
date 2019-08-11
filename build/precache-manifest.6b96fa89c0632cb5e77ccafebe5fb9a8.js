b5e73f5c654f2c87dab0f42744ad8a704b091e5d
laxmikant315 <laxmikantphadke@gmail.com>
2019-08-11 14:40:39 +0530

    deploy

---
+++ build/precache-manifest.6b96fa89c0632cb5e77ccafebe5fb9a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874619bfa371a1d7cd9f01b6661eeb1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b8c14aee3b71fbabaa",
+    "url": "/static/css/4.60315194.chunk.css"
+  },
+  {
+    "revision": "841c67a68a6306b78740",
+    "url": "/static/css/main.e133cc60.chunk.css"
+  },
+  {
+    "revision": "00b8c14aee3b71fbabaa",
+    "url": "/static/js/4.4116db26.chunk.js"
+  },
+  {
+    "revision": "fcdf1dde893ae56a9b2f",
+    "url": "/static/js/5.9cde626e.chunk.js"
+  },
+  {
+    "revision": "2485369d97c955b2fcd2",
+    "url": "/static/js/generic-master.19424319.chunk.js"
+  },
+  {
+    "revision": "841c67a68a6306b78740",
+    "url": "/static/js/main.49812a67.chunk.js"
+  },
+  {
+    "revision": "de9297464c5e269c8414",
+    "url": "/static/js/one.cd5b4e22.chunk.js"
+  },
+  {
+    "revision": "36f0a57e05012437a0d8",
+    "url": "/static/js/runtime~main.2eab5b40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
